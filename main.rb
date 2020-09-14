3fcc8c327ec312fa6805bae898c1bf2a90d13658
Nikko B <ncbadoles@gmail.com>
2020-09-14 16:10:35 +0000

    check if player is still playin by creating an instance to make sure lives is still greater than 0

--- main.rb
+++ main.rb
@@ -1,9 +1,13 @@
 class Players
     attr_accessor :lives, :name
 
-    def initialize(name, lives = 1)
+    def initialize(name, lives = 3)
         @name = name
         @lives = lives
+    end
+
+    def stillPlaying
+        @lives > 0
     end
 
 end
@@ -13,10 +17,26 @@ playerName = gets.chomp
 
 playerOne = Players.new(playerName)
 
+#test 1
+=begin 
 if playerOne.lives <= 0
     puts  "#{playerOne.name} you have no lives left!"
 elsif playerOne.lives == 1
     puts "your name is #{playerOne.name} and you still have #{playerOne.lives} life left"
 else
     puts "your name is #{playerOne.name} and you still have #{playerOne.lives} lives"
+end 
+=end
+
+# test 2
+while playerOne.stillPlaying do
+    playerOne.lives -= 1
+
+    if playerOne.lives <= 0
+        puts  "#{playerOne.name} youre dead! "
+    elsif playerOne.lives == 1
+        puts "your name is #{playerOne.name} and you still have #{playerOne.lives} life left"
+    else
+        puts "your name is #{playerOne.name} and you still have #{playerOne.lives} lives"
+    end 
 end
