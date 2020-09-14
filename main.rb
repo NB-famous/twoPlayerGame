3c4ee057d2b824f3018bc81407aa209591a88d8b
Nikko B <ncbadoles@gmail.com>
2020-09-14 18:00:06 +0000

    succeded in creating a class for questions that initialize the value of the questions and answer for  the sum of those values

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+# Player class holds the name and lives of each player
 class Players
     attr_accessor :lives, :name
 
@@ -12,12 +13,24 @@ class Players
 
 end
 
+class Questions
+    attr_accessor :answer, :val1, :val2
+    
+    def initialize
+        #Generate random value between 1 to 10    
+        @val1 = rand(1...10)
+        @val2 = rand(1...10)
+        @answer = @val1 + @val2
+    end
+end
+
+
 # Get the info from the terminal
 playerName = gets.chomp
 
 playerOne = Players.new(playerName)
 
-#test 1
+#test 1 check if info can be retrieve from class players
 =begin 
 if playerOne.lives <= 0
     puts  "#{playerOne.name} you have no lives left!"
@@ -28,7 +41,8 @@ else
 end 
 =end
 
-# test 2
+# test 2 check if player is still playing or no more lives
+=begin 
 while playerOne.stillPlaying do
     playerOne.lives -= 1
 
@@ -39,4 +53,25 @@ while playerOne.stillPlaying do
     else
         puts "your name is #{playerOne.name} and you still have #{playerOne.lives} lives"
     end 
+end 
+=end
+
+# test 3 See if we can create a condition where when output a question and input a answer(.gets.chomp)
+while playerOne.stillPlaying do
+    question = Questions.new
+    puts "#{playerOne.name}: What is the sum of #{question.val1} and #{question.val2}"
+    print "What's your answer: "
+    answer = gets.chomp
+    if answer.to_i == question.answer && playerOne.lives == 3
+        puts "Success!"
+        puts "You still have #{playerOne.lives} lives left"
+    elsif answer.to_i == question.answer && playerOne.lives > 0  
+        playerOne.lives -= 1
+        puts "lost a life"
+        puts "You still have #{playerOne.lives} lives left"
+    else
+        playerOne.lives -= 1
+        puts "Game Over!!!"
+    end
+
 end
